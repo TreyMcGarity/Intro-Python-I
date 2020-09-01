f8d4f520fc1156b276c72eb8a0c7a917cd88d4e3
Trey McGarity <trey.mcgarity99@gmail.com>
2020-09-01 07:46:57 -0500

    completed stretch

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,5 +1,5 @@
 num = input("Input a number to see if it's prime.")
-if int(num) % 2 == 0:
+if int(num) % 2 == 0 and int(num) % 3 == 0 and int(num) % 5 == 0 and int(num) % 7 == 0:
     print("Not prime.")
 else:
     print("Prime!")
